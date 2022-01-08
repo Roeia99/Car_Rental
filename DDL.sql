cd4b25f4499e5dc9dd4b20bf03a42e7e34c9333c
Omar Mokhtar <omokhtarm@gmail.com>
2022-01-08 22:35:08 +0200

    the magical ddl.

--- DDL.sql
+++ DDL.sql
@@ -19,14 +19,14 @@ CREATE TABLE office
 CREATE TABLE customer
 (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name  VARCHAR(20) NOT NULL,
-    last_name   VARCHAR(20) NOT NULL,
-    Street_name VARCHAR(20) NOT NULL,
-    city        VARCHAR(20) NOT NULL,
-    country     VARCHAR(20) NOT NULL,
-    email       VARCHAR(50) NOT NULL,
-    `password`  VARCHAR(20) NOT NULL,
-    phone_no    VARCHAR(20) NOT NULL
+    first_name  VARCHAR(255) NOT NULL,
+    last_name   VARCHAR(255) NOT NULL,
+    Street_name VARCHAR(255) NOT NULL,
+    city        VARCHAR(255) NOT NULL,
+    country     VARCHAR(255) NOT NULL,
+    email       VARCHAR(255) NOT NULL,
+    `password`  VARCHAR(255) NOT NULL,
+    phone_no    VARCHAR(255) NOT NULL
 );
 
 CREATE Table reservation
